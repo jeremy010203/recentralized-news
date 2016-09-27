164b2a38cb5872f6517978c5360fe39d340ee0bc
Jérémy Lugand <lugand_j@epita.fr>
2016-09-27 15:45:43 +0200

    [ADD] basic grid support in bootstrap

--- src/flask/app.py
+++ src/flask/app.py
@@ -4,9 +4,34 @@ import re
 
 app = Flask(__name__)
 
+def build_2x2_grid(c1, c2, c3, c4):
+    return build_container (build_row (build_col_md(c1, 6) + build_col_md(c2, 6)) + build_row (build_col_md(c3, 6) + build_col_md(c4, 6)))
+
+def build_container(content):
+    return '<div class="container-fluid">' + content + '</div>'
+
+def build_col_md(content, nb):
+    return '<div class="col-md-' + str(nb) + '">'+ content + '</div>'
+
+def build_row(content):
+    return '<div class="row">'+ content + '</div>'
+
+def build_html(content):
+    return '''<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                  <meta charset="utf-8">
+                  <meta name="viewport"
+                     content="width=device-width, initial-scale=1, user-scalable=yes">
+                  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+                </head>
+                <body>''' + content + '</body></html>'
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return build_html(build_2x2_grid(build_2x2_grid("hello2", "hello2", "hello2", "hello2"), build_2x2_grid("hello2", "hello2", "hello2", "hello2")
+        , build_2x2_grid("hello2", "hello2", "hello2", "hello2"), build_2x2_grid("hello2", "hello2", "hello2", "hello2")))
 
 @app.route('/ping')
 def ping():
