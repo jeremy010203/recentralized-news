29f8f7aaea6391b1a2e287202e1a3384deb8b600
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-09-28 11:04:33 +0200

    Fix module content request.
    There was an error when getting the content of a module from its id.
    Issue is now fixed.

--- src/koncentrator/app.py
+++ src/koncentrator/app.py
@@ -18,9 +18,9 @@ def info_koncentrator():
     settings = reader.Settings('config.yml')
     return jsonify(settings.get_content())
 
-@app.route('/modules/<int:id>/content')
-def get_content():
-    if id == '657e4':
+@app.route('/module/<string:module_id>/content')
+def get_content(module_id):
+    if module_id == '657e4':
         return '<html>Hello World from module</html>'
     else:
-        return '<html>Error! Module not found', 404
+        return '<html>Error! Module not found</html>', 404
