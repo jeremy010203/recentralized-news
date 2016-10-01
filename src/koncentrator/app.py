1b0937339f2fe729f63826beb7155c16c8185908
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-01 10:43:59 +0200

    Remove old functions in app.py.
    There were some functions that were migrated into utils module left in
    app.py. This is now fixed, these functions were remove from app.py.

--- src/koncentrator/app.py
+++ src/koncentrator/app.py
@@ -20,18 +20,3 @@ if __name__ == "__main__":
     else:
         port = 80
     app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
-
-def add_module(module):
-    global modules
-    modules[module.module_id] = module
-
-def get_modules():
-    global modules
-    return modules
-
-def get_module(module_id):
-    global modules
-    if module_id in modules:
-        return modules[module_id]
-    else:
-        return None
