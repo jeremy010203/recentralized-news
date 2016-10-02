6f9923138f5cfed8650b9ff804740d83d3363d2f
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-02 16:17:59 +0200

    Update deploy script for module 'web-pages'.
    The deploy script of the module 'web-pages' has been updated in order to
    work like the other modules, view, and koncentrator.

--- src/modules/web-pages/deploy.sh
+++ src/modules/web-pages/deploy.sh
@@ -7,8 +7,18 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-sudo docker stop web-pages
-sudo docker rm web-pages
+if [ ! -n "$(sudo docker network ls | grep "$2")" ]; then
+    echo "Error: Network '$2' does not exit"
+    exit 2
+elif [ -n "$(sudo docker ps | grep "web-pages")" ]; then
+		echo "Stopping 'web-pages' container:"
+    sudo docker stop web-pages
+		echo "Removing 'web-pages' container:"
+    sudo docker rm web-pages
+elif [ -n "$(sudo docker ps -a | grep "web-pages")" ]; then
+		echo "Removing 'web-pages' container:"
+    sudo docker rm web-pages
+fi
 sudo docker run --network=$2 --name web-pages \
     -d \
     -v $1:/app \
