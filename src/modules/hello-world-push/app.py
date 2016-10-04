83a68038d8d29bc381bee4edf8f510ecc4b106d4
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-04 13:17:36 +0200

    Update module hello-world-push using token.
    The hello-world-push module now uses the given token in order to push
    its content.

--- src/modules/hello-world-push/app.py
+++ src/modules/hello-world-push/app.py
@@ -6,11 +6,12 @@ import time
 
 if __name__ == '__main__':
     out = requests.post('http://koncentrator:80/module/register',
-            json={"name":"hello-world-push","push":True,"expiration":5})
+            json={"name":"hello-world-push1","push":True,"expiration":5})
     dict = out.json()
+    print(dict)
 
     while True:
-        mod_id = dict['id']
+        mod_id = dict['token']
         url = "http://koncentrator:80/module/" + mod_id + "/content"
         out = requests.post(url, json={"content":"<html>Hello World!</html>"})
         time.sleep(10)
