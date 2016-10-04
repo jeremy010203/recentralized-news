7dbd6f9dcda4dcf87e5a0da201ac544d0aeb7dd8
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-04 22:40:35 +0200

    Replace URL '/info/list/module' by '/module/list'.
    The previous URL did not have much sens as all modules are accessed
    through '/module/<id>/*'. This is now fixed with the new URL
    '/module/list'.

--- src/koncentrator/view_api/view.py
+++ src/koncentrator/view_api/view.py
@@ -3,18 +3,18 @@ from utils import utils
 
 api = Blueprint('api', __name__)
 
-@api.route('/info/list/module')
+@api.route('/info/koncentrator')
+def info_koncentrator():
+    settings = reader.Settings('config.yml')
+    return jsonify(settings.get_content())
+
+@api.route('/module/list')
 def list_module():
     dict = {}
     modules = utils.get_modules()
     for module_key in modules:
         dict[module_key] = modules[module_key].module_name
     return jsonify(dict)
-
-@api.route('/info/koncentrator')
-def info_koncentrator():
-    settings = reader.Settings('config.yml')
-    return jsonify(settings.get_content())
 
 @api.route('/module/<string:module_id>/content')
 def get_content(module_id):
