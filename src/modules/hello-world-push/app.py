7fdf8e0474cc083a7758d47c922632acd06dd03e
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-04 22:19:14 +0200

    Update module 'hello-world-push'.
    This update shows how to manage a module that could possibly have its
    content outdated as it is still running. This is the reason why a Flask
    server and a thread that runs the loop are started.
    The server is started in order to handle requests in the case the
    content has expired. The thread is used in order to regularly push
    content on the koncentrator.

--- src/modules/hello-world-push/app.py
+++ src/modules/hello-world-push/app.py
@@ -3,15 +3,26 @@
 from flask import Flask, jsonify
 import requests
 import time
+import threading
+
+app = Flask(__name__)
+
+@app.route('/content')
+def main():
+    return "Hello world!"
+
+def loop():
+    while True:
+        mod_id = dict['token']
+        url = "http://koncentrator:80/module/" + mod_id + "/content"
+        out = requests.post(url, json={"content":"<html>Hello World!</html>"})
+        time.sleep(10)
 
 if __name__ == '__main__':
     out = requests.post('http://koncentrator:80/module/register',
             json={"name":"hello-world-push","push":True,"expiration":5})
     dict = out.json()
     print(dict)
-
-    while True:
-        mod_id = dict['token']
-        url = "http://koncentrator:80/module/" + mod_id + "/content"
-        out = requests.post(url, json={"content":"<html>Hello World!</html>"})
-        time.sleep(10)
+    thread = threading.Thread(target=loop, daemon=True)
+    thread.start()
+    app.run(host='0.0.0.0', port=80, threaded=True)
