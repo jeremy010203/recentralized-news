bf9458b6e701caaf068b5a70f4c1a48384b91c87
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-14 00:46:32 +0200

    Add new functions in utils for managing the views.
    A new dict is initialized in order to add views into a global variable.
    This will be used when views will need to login in order to get some
    content or do more advanced actions.

--- src/koncentrator/utils/utils.py
+++ src/koncentrator/utils/utils.py
@@ -1,6 +1,9 @@
 global modules
 modules = {}
 
+global views
+views = {}
+
 def add_module(module):
     global modules
     modules[module.module_id] = module
@@ -23,3 +26,22 @@ def remove_module(module_id):
     global modules
     if module_id in modules:
         modules.pop(module_id)
+
+def add_view(view):
+    global views
+    views[view.id] = view
+
+def get_views():
+    global views
+    return views
+
+def get_view(view_id):
+    global views
+    if view_id in views:
+        return views[view_id]
+    return None
+
+def remove_view(view_id):
+    global views
+    if view_id in views:
+        views.pop(view_id)
