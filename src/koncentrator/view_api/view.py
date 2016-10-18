95254ec55392e933fe08ec6a3d901d06d56963ae
Paul-Henry Perrissel <paulhenry.perrissel.stag@axa.com>
2016-10-18 23:47:43 +0200

    Add POST method on modules content.
    This function is used for registered views in order to do a better cache
    management and make faster for the views to update or not their
    content. The view is still polling a specific module.
    In the case of a push module, the Koncentrator is able to tell if the
    current view has already polled it and if it needed that the view
    updates its content.

--- src/koncentrator/view_api/view.py
+++ src/koncentrator/view_api/view.py
@@ -1,6 +1,7 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from utils import utils
 import uuid
+import time
 
 api = Blueprint('api', __name__)
 
@@ -17,7 +18,54 @@ def list_module():
         dict[module_key] = modules[module_key].module_name
     return jsonify(dict)
 
-@api.route('/module/<string:module_id>/content')
+@api.route('/module/<string:module_id>/content', methods=['POST'])
+def post_content(module_id):
+    module = utils.get_module(module_id=module_id)
+    view = utils.get_view(request.json['token'])
+    answer = {}
+    if view is None:
+        answer['changed'] = True
+        answer['data'] = "You are not able to do this"
+        answer['error'] = True
+        return jsonify(answer), 403
+    if module is None:
+        answer['changed'] = True
+        answer['data'] = "This id does not refer any module"
+        answer['error'] = True
+        return jsonify(answer), 404
+    answer['error'] = False
+    if module.push_method:
+        content = module.get_last_content()
+        if content is None:
+            answer['changed'] = False
+            answer['data'] = "Module has not pushed anything yet"
+            answer['error'] = True
+            return jsonify(answer), 404
+        elif content.is_expired():
+            answer['changed'] = False
+            answer['data'] = "Content for this module has expired"
+            answer['error'] = True
+            utils.remove_module(module_id)
+            return jsonify(answer), 404
+        elif module_id in view.polled:
+            if view.polled[module_id] < content.receive_time:
+                answer['changed'] = True
+                answer['data'] = content.raw_content
+                view.polled[module_id] = int(time.time())
+        else:
+            view.polled[module_id] = int(time.time())
+            answer['changed'] = True
+            answer['data'] = content.raw_content
+    else:
+        content = module.get_content()
+        answer['content'], answer['changed'] = module.get_content()
+        answer['error'] = not answer['changed']
+        if answer['error']:
+            answer['data'] = "Module has disconnected"
+            return jsonify(answer), 404
+    return jsonify(answer)
+
+@api.route('/module/<string:module_id>/content', methods=['GET'])
 def get_content(module_id):
     answer = {}
     answer['id'] = module_id
